aa46f806d03f1d43d168b58a1d283f72427aec83
Chaplin16 <chaplin16@orange.fr>
2021-06-26 23:51:46 +0200

    refacto photos

--- js/items.js
+++ js/items.js
@@ -26,8 +26,34 @@ const items = [
         alt:"photo de l'interieur d'un sac de plage en tissu blanc'"
     },
     {
+        id:6,
+        attachment:"images/items/sac_bleu.jpeg",
+        alt:"photo d'un jolie sac bleu"
+    },
+    {
         id:7,
+        attachment:"images/items/masque_dead.jpeg",
+        alt:"photo d'un masque tête de mort"
+    },
+    {
+        id:8,
+        attachment:"images/items/masque_green.jpeg",
+        alt:"photo d'un masque effet vegetation"
+    },
+    {
+        id:9,
+        attachment:"images/items/masque_flamingo.jpeg",
+        alt:"photo d'un masque flamants roses"
+    },
+    {
+        id:10,
+        attachment:"images/items/seal_bag.jpeg",
+        alt:"photo d'un sac sceau"
+    },
+    {
+        id:11,
         attachment:"images/items/interieur_boutique_deco.jpg",
         alt:"photo de l'interieur de la boutique"
-    }
+    },
+
 ]
